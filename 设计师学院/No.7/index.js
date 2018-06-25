38470a4ae10dca4c564737a1dcaf4051a50e128a
xiaotiandada <952822399@qq.com>
2018-06-26 00:15:04 +0800

    增加下划线效果

--- 设计师学院/No.7/index.js
+++ 设计师学院/No.7/index.js
@@ -6,28 +6,41 @@ $(function () {
     var formLabelEmail = $('#form__label__email')
     var formLabelPassword = $('#form__label__password')
 
+
     email.focus(function () {
         if (!(email.val().length)) {
             formLabelEmail.addClass('active')
+
         }
+       $('.form__wrapper:eq(0)').addClass('form__wrapper__active')
+
     })
 
     email.blur(function () {
         if (!(email.val().length)) {
             formLabelEmail.removeClass('active')
+
         }
+        $('.form__wrapper:eq(0)').removeClass('form__wrapper__active')
+
     })
 
     password.focus(function () {
         if (!(password.val().length)) {
             formLabelPassword.addClass('active')
+
         }
+       $('.form__wrapper:eq(1)').addClass('form__wrapper__active')
+
     })
 
     password.blur(function () {
         if (!(password.val().length)) {
             formLabelPassword.removeClass('active')
         }
+
+       $('.form__wrapper:eq(1)').removeClass('form__wrapper__active')
+
     })
 
     email.change(function(){
